81fd2afc84ed4bc7bd1974791c738604d0134268
SK Yurievich auto-pipeline <auto@sk-yurievich.ru>
2026-06-16 13:33:58 +0000

    blog: daily 5 SEO articles 2026-06-16
    slugs:   - plitnyy-fundament-gatchina-cena
      - plitnyy-fundament-10x10-cena-pod-klyuch-spb
      - kak-zalit-plitnyy-fundament-zimoy
      - svai-ili-plita-pod-gazobeton-leningradskaya-oblast
      - gruntovye-vody-blizko-kakoy-fundament-vybrat

--- lib/articles/index.ts
+++ lib/articles/index.ts
@@ -1,18 +1,28 @@
 import type { Article } from './_types';
+import { gruntovyeVodyBlizkoKakoyFundamentVybrat } from './gruntovye-vody-blizko-kakoy-fundament-vybrat';
+import { kakZalitPlitnyyFundamentZimoy } from './kak-zalit-plitnyy-fundament-zimoy';
 import { plitaIliLentaPodGazobeton } from './plita-ili-lenta-pod-gazobeton';
+import { plitnyyFundament10x10CenaPodKlyuchSpb } from './plitnyy-fundament-10x10-cena-pod-klyuch-spb';
+import { plitnyyFundamentGatchinaCena } from './plitnyy-fundament-gatchina-cena';
 import { plitnyyFundamentNaTorfeLeskolovoNaziya } from './plitnyy-fundament-na-torfe-leskolovo-naziya';
 import { plitnyyFundamentPodGazobetonTolschinaArmirovanie } from './plitnyy-fundament-pod-gazobeton-tolschina-armirovanie';
 import { plitnyyFundamentVsevolozhskCena } from './plitnyy-fundament-vsevolozhsk-cena';
+import { svaiIliPlitaPodGazobetonLeningradskayaOblast } from './svai-ili-plita-pod-gazobeton-leningradskaya-oblast';
 import { treschinyVFundamentePrichinyChtoDelat } from './treschiny-v-fundamente-prichiny-chto-delat';
 
 // Реестр статей блога. Авто-сгенерирован import-articles.js
 // Свежие статьи добавляются сюда автоматически.
 
 export const ARTICLES: Article[] = [
+  gruntovyeVodyBlizkoKakoyFundamentVybrat,
+  kakZalitPlitnyyFundamentZimoy,
   plitaIliLentaPodGazobeton,
+  plitnyyFundament10x10CenaPodKlyuchSpb,
+  plitnyyFundamentGatchinaCena,
   plitnyyFundamentNaTorfeLeskolovoNaziya,
   plitnyyFundamentPodGazobetonTolschinaArmirovanie,
   plitnyyFundamentVsevolozhskCena,
+  svaiIliPlitaPodGazobetonLeningradskayaOblast,
   treschinyVFundamentePrichinyChtoDelat,
 ];
 
